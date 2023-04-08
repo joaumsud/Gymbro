c34d269131c8fabda19b49b9ba326da89f353f73
Marco Vinicius <marco.paula@liax.tech>
2023-04-08 12:15:03 -0300

    Implementa maps

--- gymbro-front/src/pages/HomeUseAuth/index.tsx
+++ gymbro-front/src/pages/HomeUseAuth/index.tsx
@@ -1,18 +1,66 @@
-import { logout } from "../../services/auth.service";
-import { useHistory } from "react-router-dom";
+import { Grid, TextField } from "@mui/material";
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { useCallback,useState } from "react";
+import { getToken } from "../../services/auth.service";
+
+const containerStyle = {
+    width: '400px',
+    height: '400px'
+};
+
+const center = {
+    lat: -3.745,
+    lng: -38.523
+};
 
 const Dash = () => {
-    const history = useHistory()
+
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: getToken()
+    })
+
+    const [map, setMap] = useState(null)
+
+    const onLoad = useCallback(function callback(map) {
+        const bounds = new window.google.maps.LatLngBounds(center);
+        map.fitBounds(bounds);
+
+        setMap(map)
+    }, [])
+
+    const onUnmount = useCallback(function callback(map: any) {
+        setMap(null)
+    }, [])
+
+
     return (
         <>
-            <h1>Bem-Vindo ao GymBro</h1>
-            <button onClick={()=>{
-                logout()
-                history.push('/')
-                window.location.reload()
-            }}>
-                Sair
-            </button>
+            <Grid container >
+                <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center' }}>
+                    <TextField type="text" placeholder="Localização" />
+                </Grid>
+                <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center' }}>
+                    <TextField type="text" placeholder="Tipo de Evento" />
+                </Grid>
+                <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center' }}>
+
+                    {
+                        isLoaded ? (
+                            <GoogleMap
+                                mapContainerStyle={containerStyle}
+                                center={center}
+                                zoom={10}
+                                onLoad={onLoad}
+                                onUnmount={onUnmount}
+                            >
+                                <></>
+                            </GoogleMap>
+                        ) : <></>
+                    }
+
+                </Grid>
+            </Grid>
         </>
     );
 }
