d4c824ffa56c03be76a494b9ee3e46a39a0c0766
Marco Vinicius <marco.paula@liax.tech>
2023-05-22 11:48:17 -0300

    Refaz home

--- gymbro-front/src/components/OnlineFriends/index.tsx
+++ gymbro-front/src/components/OnlineFriends/index.tsx
@@ -1,35 +1,209 @@
+import { Box } from "@mui/material"
 
-
-const OnlineFriends = () => {
+const OnlineFriends: React.FC = () => {
     return (
-        <div style={{ border: '1px solid red', overflowY: 'scroll',height:'90vh' }}>
-            <p>teste</p>
-            <p>teste</p>
-            <p>teste</p>
-            <p>teste</p>
-            <p>teste</p>
-            <p>teste</p>
-            <p>teste</p>
-            <p>teste</p>
-            <p>teste</p>
-            <p>teste</p>
-            <p>teste</p>
-            <p>teste</p>
-            <p>teste</p>
-            <p>teste</p>
-            <p>teste</p>
-            <p>teste</p>
-            <p>teste</p>
-            <p>teste</p>
-            <p>teste</p>
-            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-            <p>teste</p>
-            <p>teste</p>
-            <p>teste</p>
-            <p>teste</p>
-            <p>teste</p>
-            
-        </div>
+        <Box style={{ overflowY: 'scroll', height: '90vh' }} sx={{ py: 2 }}>
+            <Box sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
+                <Box sx={{
+                    border: '3px solid #64FD58',
+                    width: '3rem',
+                    height: '3rem',
+                    borderRadius: '50%',
+                    m: 1,
+                    alignSelf: 'center'
+                }} >
+                    <img
+                        src="https://picsum.photos/200/200"
+                        loading="lazy"
+                        style={{
+                            objectFit: 'cover', width: '100%',
+                            height: '100%',
+                            borderRadius: '50%'
+                        }}
+                    />
+                </Box>
+                <Box sx={{
+                    border: '3px solid #64FD58',
+                    width: '3rem',
+                    height: '3rem',
+                    borderRadius: '50%',
+                    m: 1,
+                    alignSelf: 'center'
+                }} >
+                    <img
+                        src="https://picsum.photos/201/201"
+                        loading="lazy"
+                        style={{
+                            objectFit: 'cover', width: '100%',
+                            height: '100%',
+                            borderRadius: '50%'
+                        }}
+                    />
+                </Box>
+                <Box sx={{
+                    border: '3px solid #64FD58',
+                    width: '3rem',
+                    height: '3rem',
+                    borderRadius: '50%',
+                    m: 1,
+                    alignSelf: 'center'
+                }} >
+                    <img
+                        src="https://picsum.photos/199/199"
+                        loading="lazy"
+                        style={{
+                            objectFit: 'cover', width: '100%',
+                            height: '100%',
+                            borderRadius: '50%'
+                        }}
+                    />
+                </Box>
+                <Box sx={{
+                    border: '3px solid #64FD58',
+                    width: '3rem',
+                    height: '3rem',
+                    borderRadius: '50%',
+                    m: 1,
+                    alignSelf: 'center'
+                }} >
+                    <img
+                        src="https://picsum.photos/198/198"
+                        loading="lazy"
+                        style={{
+                            objectFit: 'cover', width: '100%',
+                            height: '100%',
+                            borderRadius: '50%'
+                        }}
+                    />
+                </Box>
+                <Box sx={{
+                    border: '3px solid #64FD58',
+                    width: '3rem',
+                    height: '3rem',
+                    borderRadius: '50%',
+                    m: 1,
+                    alignSelf: 'center'
+                }} >
+                    <img
+                        src="https://picsum.photos/202/202"
+                        loading="lazy"
+                        style={{
+                            objectFit: 'cover', width: '100%',
+                            height: '100%',
+                            borderRadius: '50%'
+                        }}
+                    />
+                </Box>
+                <Box sx={{
+                    border: '3px solid #64FD58',
+                    width: '3rem',
+                    height: '3rem',
+                    borderRadius: '50%',
+                    m: 1,
+                    alignSelf: 'center'
+                }} >
+                    <img
+                        src="https://picsum.photos/202/202"
+                        loading="lazy"
+                        style={{
+                            objectFit: 'cover', width: '100%',
+                            height: '100%',
+                            borderRadius: '50%'
+                        }}
+                    />
+                </Box>
+                <Box sx={{
+                    border: '3px solid #64FD58',
+                    width: '3rem',
+                    height: '3rem',
+                    borderRadius: '50%',
+                    m: 1,
+                    alignSelf: 'center'
+                }} >
+                    <img
+                        src="https://picsum.photos/202/202"
+                        loading="lazy"
+                        style={{
+                            objectFit: 'cover', width: '100%',
+                            height: '100%',
+                            borderRadius: '50%'
+                        }}
+                    />
+                </Box>
+                <Box sx={{
+                    border: '3px solid #64FD58',
+                    width: '3rem',
+                    height: '3rem',
+                    borderRadius: '50%',
+                    m: 1,
+                    alignSelf: 'center'
+                }} >
+                    <img
+                        src="https://picsum.photos/202/202"
+                        loading="lazy"
+                        style={{
+                            objectFit: 'cover', width: '100%',
+                            height: '100%',
+                            borderRadius: '50%'
+                        }}
+                    />
+                </Box>
+                <Box sx={{
+                    border: '3px solid #64FD58',
+                    width: '3rem',
+                    height: '3rem',
+                    borderRadius: '50%',
+                    m: 1,
+                    alignSelf: 'center'
+                }} >
+                    <img
+                        src="https://picsum.photos/202/202"
+                        loading="lazy"
+                        style={{
+                            objectFit: 'cover', width: '100%',
+                            height: '100%',
+                            borderRadius: '50%'
+                        }}
+                    />
+                </Box>
+                <Box sx={{
+                    border: '3px solid #64FD58',
+                    width: '3rem',
+                    height: '3rem',
+                    borderRadius: '50%',
+                    m: 1,
+                    alignSelf: 'center'
+                }} >
+                    <img
+                        src="https://picsum.photos/202/202"
+                        loading="lazy"
+                        style={{
+                            objectFit: 'cover', width: '100%',
+                            height: '100%',
+                            borderRadius: '50%'
+                        }}
+                    />
+                </Box>
+                <Box sx={{
+                    border: '3px solid #64FD58',
+                    width: '3rem',
+                    height: '3rem',
+                    borderRadius: '50%',
+                    m: 1,
+                    alignSelf: 'center'
+                }} >
+                    <img
+                        src="https://picsum.photos/202/202"
+                        loading="lazy"
+                        style={{
+                            objectFit: 'cover', width: '100%',
+                            height: '100%',
+                            borderRadius: '50%'
+                        }}
+                    />
+                </Box>
+            </Box>
+        </Box>
     )
 }
 
--- gymbro-front/src/pages/HomeUseAuth/index.tsx
+++ gymbro-front/src/pages/HomeUseAuth/index.tsx
@@ -2,24 +2,199 @@ import './style.css'
 import MapEvents from '../../components/MapEvents';
 import "leaflet/dist/leaflet.css";
 import { useUserAuth } from '../../hooks/userProvider';
-import { Grid } from '@mui/material';
+import { Box, Button, CssBaseline, Divider, Drawer, Grid, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, SwipeableDrawer, Toolbar, Typography, styled, useTheme } from '@mui/material';
 import OnlineFriends from '../../components/OnlineFriends';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import MenuIcon from '@mui/icons-material/Menu';
+import ListIcon from '@mui/icons-material/List';
+import React, { useState } from 'react';
+
+const drawerWidth = 240;
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })<{
+    open?: boolean;
+}>(({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginRight: -drawerWidth,
+    ...(open && {
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginRight: 0,
+    }),
+}));
+
+interface AppBarProps extends MuiAppBarProps {
+    open?: boolean;
+}
+
+const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({ theme, open }) => ({
+    transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginRight: drawerWidth,
+    }),
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-start',
+}));
 
 const Dash = () => {
     const { user } = useUserAuth()
+
+    const theme = useTheme();
+
+    const [open, setOpen] = useState(false);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
 
     return (
         <>
             <Grid container style={{
             }}>
-                <Grid item lg={1} md={1} sm={12} xs={12} style={{ border: '1px solid red' }}>
+                <Grid item lg={1} md={1} sm={12} xs={12}>
                     <OnlineFriends />
                 </Grid>
-                <Grid item lg={11} md={11} sm={12} xs={12}>
+                <Grid item lg={11} md={11} sm={12} xs={12} sx={{position:'relative'}}>
                     <MapEvents />
-                </Grid>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="end"
+                        onClick={handleDrawerOpen}
+                        sx={{ ...(open && { display: 'none' }) }}
 
+                        style={{ position: 'absolute', top: '3%', zIndex: '1000', right: '0%', left: '97%' }}
+                    >
+                        <ListIcon />
+                    </IconButton>
+                </Grid>
             </Grid>
+            <>
+
+                <Box sx={{ display: 'flex' }}>
+
+
+                    <CssBaseline />
+
+
+                    {/* <ListIcon style={{position: 'relative', top:'50%', zIndex:'2000'}}/> */}
+                    {/* <AppBar position="fixed" open={open}>
+                        <Toolbar>
+                            <Typography variant="h6" noWrap sx={{ flexGrow: 1 }} component="div">
+                                Persistent drawer
+                            </Typography>
+
+                        </Toolbar>
+                    </AppBar> */}
+
+                    {/* <Main open={open}>
+                        <DrawerHeader />
+                        <Typography paragraph>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
+                            enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
+                            imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
+                            Convallis convallis tellus id interdum velit laoreet id donec ultrices.
+                            Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+                            adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
+                            nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
+                            leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
+                            feugiat vivamus at augue. At augue eget arcu dictum varius duis at
+                            consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
+                            sapien faucibus et molestie ac.
+                        </Typography>
+                        <Typography paragraph>
+                            Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
+                            eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
+                            neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
+                            tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
+                            sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
+                            tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
+                            gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
+                            et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
+                            tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+                            eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
+                            posuere sollicitudin aliquam ultrices sagittis orci a.
+                        </Typography>
+                    </Main> */}
+                    <Drawer
+                        sx={{
+                            width: drawerWidth,
+                            flexShrink: 0,
+                            '& .MuiDrawer-paper': {
+                                width: drawerWidth,
+                            },
+                        }}
+                        variant="persistent"
+                        anchor="right"
+                        open={open}
+                    >
+                        <DrawerHeader>
+                            <IconButton onClick={handleDrawerClose}>
+                                {theme.direction === 'rtl' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                            </IconButton>
+                        </DrawerHeader>
+                        <Divider />
+                        <List>
+                            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                                <ListItem key={text} disablePadding>
+                                    <ListItemButton>
+                                        <ListItemIcon>
+                                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                                        </ListItemIcon>
+                                        <ListItemText primary={text} />
+                                    </ListItemButton>
+                                </ListItem>
+                            ))}
+                        </List>
+                        <Divider />
+                        <List>
+                            {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                                <ListItem key={text} disablePadding>
+                                    <ListItemButton>
+                                        <ListItemIcon>
+                                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                                        </ListItemIcon>
+                                        <ListItemText primary={text} />
+                                    </ListItemButton>
+                                </ListItem>
+                            ))}
+                        </List>
+                    </Drawer>
+                </Box>
+            </>
         </>
     );
 }
