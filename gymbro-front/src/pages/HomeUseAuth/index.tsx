b9e5fe3bd153bc91eba019b5f87b310e53ed76de
Marco Vinicius <marco.paula@liax.tech>
2023-05-22 15:16:13 -0300

    Refaz home

--- gymbro-front/src/pages/HomeUseAuth/index.tsx
+++ gymbro-front/src/pages/HomeUseAuth/index.tsx
@@ -12,6 +12,7 @@ import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import MenuIcon from '@mui/icons-material/Menu';
 import ListIcon from '@mui/icons-material/List';
 import React, { useState } from 'react';
+import SideBarFilters from '../../components/SidebarFIltersMap';
 
 const drawerWidth = 240;
 
@@ -88,113 +89,9 @@ const Dash = () => {
                 </Grid>
                 <Grid item lg={11} md={11} sm={12} xs={12} sx={{position:'relative'}}>
                     <MapEvents />
-                    <IconButton
-                        color="inherit"
-                        aria-label="open drawer"
-                        edge="end"
-                        onClick={handleDrawerOpen}
-                        sx={{ ...(open && { display: 'none' }) }}
-
-                        style={{ position: 'absolute', top: '3%', zIndex: '1000', right: '0%', left: '97%' }}
-                    >
-                        <ListIcon />
-                    </IconButton>
+                    <SideBarFilters/>
                 </Grid>
             </Grid>
-            <>
-
-                <Box sx={{ display: 'flex' }}>
-
-
-                    <CssBaseline />
-
-
-                    {/* <ListIcon style={{position: 'relative', top:'50%', zIndex:'2000'}}/> */}
-                    {/* <AppBar position="fixed" open={open}>
-                        <Toolbar>
-                            <Typography variant="h6" noWrap sx={{ flexGrow: 1 }} component="div">
-                                Persistent drawer
-                            </Typography>
-
-                        </Toolbar>
-                    </AppBar> */}
-
-                    {/* <Main open={open}>
-                        <DrawerHeader />
-                        <Typography paragraph>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                            tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-                            enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-                            imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-                            Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-                            Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-                            adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-                            nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-                            leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-                            feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-                            consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-                            sapien faucibus et molestie ac.
-                        </Typography>
-                        <Typography paragraph>
-                            Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-                            eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-                            neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-                            tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-                            sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-                            tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-                            gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-                            et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-                            tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-                            eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-                            posuere sollicitudin aliquam ultrices sagittis orci a.
-                        </Typography>
-                    </Main> */}
-                    <Drawer
-                        sx={{
-                            width: drawerWidth,
-                            flexShrink: 0,
-                            '& .MuiDrawer-paper': {
-                                width: drawerWidth,
-                            },
-                        }}
-                        variant="persistent"
-                        anchor="right"
-                        open={open}
-                    >
-                        <DrawerHeader>
-                            <IconButton onClick={handleDrawerClose}>
-                                {theme.direction === 'rtl' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-                            </IconButton>
-                        </DrawerHeader>
-                        <Divider />
-                        <List>
-                            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                                <ListItem key={text} disablePadding>
-                                    <ListItemButton>
-                                        <ListItemIcon>
-                                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                                        </ListItemIcon>
-                                        <ListItemText primary={text} />
-                                    </ListItemButton>
-                                </ListItem>
-                            ))}
-                        </List>
-                        <Divider />
-                        <List>
-                            {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                                <ListItem key={text} disablePadding>
-                                    <ListItemButton>
-                                        <ListItemIcon>
-                                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                                        </ListItemIcon>
-                                        <ListItemText primary={text} />
-                                    </ListItemButton>
-                                </ListItem>
-                            ))}
-                        </List>
-                    </Drawer>
-                </Box>
-            </>
         </>
     );
 }
