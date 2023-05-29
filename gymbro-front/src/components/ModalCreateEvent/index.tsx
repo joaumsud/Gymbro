052e5829c93ecbba1867781aec0b6b9bef263bcd
Marco Vinicius <marco.paula@liax.tech>
2023-05-29 14:27:18 -0300

    Cria forms de criação de eventos

--- gymbro-front/src/components/ModalCreateEvent/index.tsx
+++ gymbro-front/src/components/ModalCreateEvent/index.tsx
@@ -1,36 +1,85 @@
-import { Button, Modal, Box, Typography, Grid, TextField } from "@mui/material";
+import { Button, Modal, Box, Typography, Grid, TextField, Checkbox, Divider } from "@mui/material";
 import React, { useState } from "react";
 import { useForm, SubmitHandler, Controller } from "react-hook-form";
 import useStyles from "./styles";
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css';
+import { MapContainer, TileLayer, Marker, useMapEvents } from 'react-leaflet';
+
 
 const style = {
     position: 'absolute' as 'absolute',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
+    // width: 550,
+    // minWidth: 200,
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
     p: 4,
 };
 
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+
 
 const ModalCreateEvent: React.FC = () => {
     const classes = useStyles()
+
     const [open, setOpen] = useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
+
+    const [startDate, setStartDate] = useState(new Date());
     const [title, setTitle] = useState<string>('')
     const [description, setDescription] = useState<string>('')
-    const [eventDate, setEventDate] = useState<string>('')
+    const [isPublic, setIsPublic] = useState<boolean>(false)
+    const [hasLimit, setHasLimit] = useState<boolean>(false)
+    const [limitCount, setLimitCount] = useState<number | undefined>(0)
+    const [markerLocation, setMarkerLocation] = useState<[number, number] | null>(null);
 
+    const handleTitle = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+        setTitle(e.target.value)
+    }
+
+    const handleDescription = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+        setDescription(e.target.value)
+    }
+
+    const handleCheckPublic = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setIsPublic(event.target.checked);
+    };
+
+    const handleCheckHasLimit = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setHasLimit(event.target.checked);
+    };
+
+    const handleLimitCount = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+        setLimitCount(+e.target.value)
+    }
+
+    const handleMapClick = (event: L.LeafletMouseEvent) => {
+        const { lat, lng } = event.latlng;
+        setMarkerLocation([lat, lng]);
+    };
+
+    const LocationMarker = () => {
+        useMapEvents({
+            click: handleMapClick,
+        });
+
+        if (markerLocation) {
+            return <Marker position={markerLocation} />;
+        }
+
+        return null;
+    };
 
     const { handleSubmit, control, reset } = useForm();
 
     return (
         <>
-            <div>
+            <Box >
                 <Grid>
                     <Button onClick={handleOpen} className={classes.btnAdd} >Criar Evento</Button>
                 </Grid>
@@ -40,8 +89,10 @@ const ModalCreateEvent: React.FC = () => {
                     aria-labelledby="modal-modal-title"
                     aria-describedby="modal-modal-description"
                 >
-                    <Box sx={style}>
-                        <form>
+                    <Box sx={style} className={classes.modalStyle}>
+                        <Typography variant="h6">Crie seu evento aqui!</Typography>
+                        <Divider />
+                        <form className={classes.formStyle}>
                             <Controller
                                 control={control}
                                 rules={{
@@ -49,12 +100,11 @@ const ModalCreateEvent: React.FC = () => {
                                 }}
                                 render={({ field: { onChange, onBlur, value } }) => (
                                     <TextField
+                                        className={classes.inputsStyle}
                                         label="Título"
                                         variant="outlined"
                                         value={title}
-                                        onChange={(e) => {
-                                            setTitle(e.target.value)
-                                        }}
+                                        onChange={handleTitle}
                                     />
                                 )}
                                 name="title"
@@ -67,36 +117,96 @@ const ModalCreateEvent: React.FC = () => {
                                 }}
                                 render={({ field: { onChange, onBlur, value } }) => (
                                     <TextField
+                                        className={classes.inputsStyle}
                                         label="Descrição"
                                         variant="outlined"
+                                        multiline
+                                        maxRows={4}
                                         value={description}
-                                        onChange={(e) => {
-                                            setTitle(e.target.value)
-                                        }}
+                                        onChange={handleDescription}
                                     />
                                 )}
                                 name="description"
                             />
 
-                            {/* <Controller
+                            <Controller
                                 control={control}
                                 rules={{
                                     required: true,
                                 }}
                                 render={({ field: { onChange, onBlur, value } }) => (
                                     <DatePicker
-                                        label="Controlled picker"
-                                        value={eventDate}
-                                        onChange={(e) => setEventDate(e.target.value)}
+                                        showIcon
+                                        selected={startDate}
+                                        onChange={(e) => {
+                                            if (e) setStartDate(e)
+                                        }}
                                     />
                                 )}
-                                name="description"
-                            /> */}
+                                name="date"
+                            />
 
+                            <Controller
+                                control={control}
+                                rules={{
+                                    required: true,
+                                }}
+                                render={({ field: { onChange, onBlur, value } }) => (
+                                    <Checkbox
+                                        checked={isPublic}
+                                        onChange={handleCheckPublic}
+                                        {...label}
+                                    />
+                                )}
+                                name="isPublic"
+                            />
+
+                            <Controller
+                                control={control}
+                                rules={{
+                                    required: true,
+                                }}
+                                render={({ field: { onChange, onBlur, value } }) => (
+                                    <Checkbox
+                                        checked={hasLimit}
+                                        onChange={handleCheckHasLimit}
+                                        {...label}
+                                    />
+                                )}
+                                name="hasLimit"
+                            />
+
+
+                            <Controller
+                                control={control}
+                                rules={{
+                                    required: true,
+                                }}
+                                render={({ field: { onChange, onBlur, value } }) => (
+                                    <TextField
+                                        disabled={!hasLimit ?? true}
+                                        label="Descrição"
+                                        variant="outlined"
+                                        type="number"
+                                        value={limitCount}
+                                        onChange={handleLimitCount}
+                                    />
+                                )}
+                                name="limitCount"
+                            />
+
+                            <Box>
+                                <MapContainer center={[0, 0]} zoom={2} style={{ height: '400px' }}>
+                                    <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                                    <LocationMarker />
+                                </MapContainer>
+                            </Box>
+                            <Button>Cancelar</Button>
+                            <Button type="submit">Criar Evento</Button>
                         </form>
                     </Box>
                 </Modal>
-            </div>
+            </Box>
         </>
     )
 }
--- gymbro-front/src/components/ModalCreateEvent/styles.ts
+++ gymbro-front/src/components/ModalCreateEvent/styles.ts
@@ -1,6 +1,22 @@
 import { makeStyles } from '@mui/styles'
+import theme from '../../theme'
 
 const useStyles = makeStyles({
+    modalStyle: {
+        overflowY: 'scroll',
+        height: '600px',
+        minHeight: '600px',
+        width: '550px',
+        [theme.breakpoints.down('md')]:{
+            width:'450px'
+        }
+    },
+    formStyle: {
+        padding: '1.2rem 0'
+    },
+    inputsStyle:{
+      margin:'15px 0px !important'  
+    },
     btnAdd: {
         width: '150px !important',
         padding: '10px 20px !important',
@@ -11,6 +27,7 @@ const useStyles = makeStyles({
             backgroundColor: '#110FFA !important'
         }
     }
+
 })
 
 export default useStyles
